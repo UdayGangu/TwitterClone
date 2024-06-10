88b3106b1d60bc47bb69017041dd701998133492
UdayGangu <157138131+UdayGangu@users.noreply.github.com>
2024-06-10 21:23:58 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import { v2 as cloudinary } from "cloudinary";
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
@@ -13,11 +12,6 @@ import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
 
-cloudinary.config({
-	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-	api_key: process.env.CLOUDINARY_API_KEY,
-	api_secret: process.env.CLOUDINARY_API_SECRET,
-});
 
 const app = express();
 const PORT = process.env.PORT || 5000;
